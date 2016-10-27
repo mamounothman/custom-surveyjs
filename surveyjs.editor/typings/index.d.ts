da3bcda0725046050fedc2648edd8016122a8074
Ma'moun Othman <mamoun.o@omnix.ae>
2016-10-27 18:01:48 +0300

    Remove .gitignore.

---
+++ surveyjs.editor/typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/ace/index.d.ts" />
+/// <reference path="globals/bootstrap-notify/index.d.ts" />
+/// <reference path="globals/knockout/index.d.ts" />
+/// <reference path="globals/qunit/index.d.ts" />
+/// <reference path="globals/surveyjs/index.d.ts" />
